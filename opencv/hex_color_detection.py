20ea4223f9aaef857ba364e994b28d579158b6b1
Arkin Mukherjee <amukherjee@benunets.com>
2020-01-25 17:24:57 -0500

    Fix file path for sample image to be relative instead of absolute

--- opencv/hex_color_detection.py
+++ opencv/hex_color_detection.py
@@ -1,15 +1,15 @@
-import numpy
+# import numpy
 import cv2
 
 #intakes x and y coordinates
-x = input("Enter x coordinate: ")
-y = input("Enter y coordinate: ")
+x = int(input("Enter an integer for the x coordinate: "))
+y = int(input("Enter an integer for the y coordinate: "))
 
 print (x)
 print (y)
 
 # opens image file
-image = cv2.imread("../images/IMG_0675.jpg")
+image = cv2.imread("../images/1.jpeg")
 image = cv2.resize(image, (340,480))
 
 # shows image and waits til key is pressed to close
@@ -18,11 +18,8 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # prints color as tuple
-for x in range (0,340,1):
-    for y in range(0,480,1):
-        color = image[y,x]
-        print (color)
-# color = image[y,x]
+color = image[y, x]
+print (color)
 
 # prints hexColor
 hexColor = (color[0] << 16) + (color[1] << 8) + (color[2])
