942fe9472d70c8ebbff80f23d61ac9736639d626
Arkin Mukherjee <amukherjee@benunets.com>
2020-02-02 16:13:40 -0500

    Implement function to detect blur in PlateImage.py

--- opencv/blur_detection.py
+++ opencv/blur_detection.py
@@ -10,36 +10,36 @@ def variance_of_laplacian(image):
     # measure, which is simply the variance of the Laplacian
     return cv2.Laplacian(image, cv2.CV_64F).var()
 
+def is_blurry():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--images", required=True,
+                    help="path to input directory of images")
+    ap.add_argument("-t", "--threshold", type=float, default=200.0,
+                    help="focus measures that fall below this value will be considered 'blurry'")
+    args = vars(ap.parse_args())
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", required=True,
-                help="path to input directory of images")
-ap.add_argument("-t", "--threshold", type=float, default=200.0,
-                help="focus measures that fall below this value will be considered 'blurry'")
-args = vars(ap.parse_args())
+    # loop over the input images
+    for imagePath in paths.list_images("../images"):
+        # load the image, convert it to grayscale, and compute the
+        # focus measure of the image using the Variance of Laplacian
+        # method
+        print("In imagepath function")
+        image = cv2.imread(imagePath)
+        print(imagePath)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        fm = variance_of_laplacian(gray)
+        text = "Not Blurry"
 
-# loop over the input images
-for imagePath in paths.list_images("../images"):
-    # load the image, convert it to grayscale, and compute the
-    # focus measure of the image using the Variance of Laplacian
-    # method
-    print("In imagepath function")
-    image = cv2.imread(imagePath)
-    print(imagePath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    fm = variance_of_laplacian(gray)
-    text = "Not Blurry"
+        # if the focus measure is less than the supplied threshold,
+        # then the image should be considered "blurry"
+        if fm < args["threshold"]:
+            text = "Blurry"
 
-    # if the focus measure is less than the supplied threshold,
-    # then the image should be considered "blurry"
-    if fm < args["threshold"]:
-        text = "Blurry"
+        print(text + ". Score: " + str(fm))
 
-    print(text + ". Score: " + str(fm))
-
-    # show the image
-    cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-    cv2.imshow("Image", image)
-    key = cv2.waitKey(0)
+        # show the image
+        cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+        cv2.imshow("Image", image)
+        key = cv2.waitKey(0)
