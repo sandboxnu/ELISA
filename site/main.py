90c549c3b93446801b9e1f8ca4e2b22e5e2d72f9
Ubuntu <ubuntu@ip-172-31-94-170.ec2.internal>
2020-04-12 17:21:29 +0000

    updated ip for hosting

--- site/main.py
+++ site/main.py
@@ -86,4 +86,4 @@ def uploaded_file(filename):
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=80)
